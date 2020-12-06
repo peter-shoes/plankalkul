c167a7fe3f1810d79f58ba294af73ab7dcff44a1
peter-shoes <bruce.mcculloch64@gmail.com>
2020-12-06 13:29:51 -0500

    oops! forgot to actually upload for 3 months!

--- t-oken.py
+++ t-oken.py
@@ -4,12 +4,25 @@ import sys
 file = open(str(sys.argv[1]), 'r')
 lines = file.read().splitlines()
 
-class AddToken():
-    def __init__(self,type):
-        self.type = type
-        # self.line_num = line_num
-        # add the other handlers here
-        pass
+class Operators():
+    def __init__(self):
+        self.ops = {
+            '(':AddToken('OPEN_PARENTHESES'),
+            ')':AddToken('CLOSE_PARENTHESES'),
+            ',':AddToken('COMMA'),
+            '+':AddToken('ADD'),
+            '-':AddToken('GUARD' if match('>') else 'SUBTRACT'),
+            '*':AddToken('MULTIPLY'),
+            '/':AddToken('XOR' if match('~') else 'DIVIDE'),
+            '&':AddToken('CONJUNCTION'),
+            '|':AddToken('DISCONJUNCTION'),
+            '!':AddToken('NEGATION'),
+            '>':AddToken('GREATER_THAN'),
+            '<':AddToken('LESS_THAN'),
+            '=':AddToken('ASSIGNMENT' if match('>') else 'EQUAL_TO'),
+        }
+
+
 
 def match(expected):
     if line[pointer+1]==expected:
@@ -18,47 +31,42 @@ def match(expected):
     else:
         return False
 
-def main():
-    tokens = []
-    # this is some beautiful fucking regex i'll tell you what
-    var_pattern = '[VZR]\d+\[(?:\d+\.+)*\d*\:(?:\d+\.+)*0\]'
-    var_re = re.compile(var_pattern)
+# this is some beautiful fucking regex i'll tell you what
+var_pattern = '[VZRi]\d+\[(?:\d+\.+)*\d*\:(?:\d+\.+)*0\]'
+var_re = re.compile(var_pattern)
+single_tokens = ['(',')',',','+','*','&','|','!','>','<']
 
-    ops_dict = {
-        '(':AddToken('OPEN_PARENTHESES'),
-        ')':AddToken('CLOSE_PARENTHESES'),
-        ',':AddToken('COMMA'),
-        '+':AddToken('ADD'),
-        '-':AddToken('GUARD' if match('>') else 'SUBTRACT'),
-        '*':AddToken('MULTIPLY'),
-        '/':AddToken('XOR' if match('~') else 'DIVIDE'),
-        '&':AddToken('CONJUNCTION'),
-        '|':AddToken('DISCONJUNCTION'),
-        '!':AddToken('NEGATION'),
-        '>':AddToken('GREATER_THAN'),
-        '<':AddToken('LESS_THAN'),
-        '=':AddToken('ASSIGNMENT' if match('>') else 'EQUAL_TO'),
-    }
 
-    line_num = 0
-    for line in lines:
-        vars_in_line = var_re.findall(line)
-        line_tokens = []
-        pointer = 0
-        while pointer <= len(line):
-            if line[pointer] in ops_dict.keys():
-                line_tokens.append(ops_dict[line[pointer]])
-                pointer+=1
-            elif line[pointer] == ('V' or 'Z' or 'R'):
-                line_tokens.append(AddToken(vars_in_line[0]))
-                pointer+=len(vars_in_line[0])
-                vars_in_line.pop(0)
+tokens = []
+line_num = 0
+for line in lines:
+    vars_inline = re.findall(var_re,line)
+    line_tokens = []
+    pointer = 0
+    while pointer < len(line)-1:
+        char = line[pointer]
+        if char in single_tokens:
+            line_tokens.append(char)
+            pointer +=1
+        elif re.match('[-=]',char):
+            if re.match('>',line[pointer+1]):
+                full_token = char+'>'
+                line_tokens.append(full_token)
+                pointer +=2
             else:
-                print(line[pointer])
-                pointer+=1
-        line_num += 1
-        tokens.append(line_tokens)
-    print(tokens)
+                line_tokens.append(char)
+                pointer +=1
+        elif re.match()
+        elif re.match('[VZRi]',char):
+            # here we need to check if it matches a variable in the line
+            # if yes, append to tokens
+            # if no, throw error
+            pass
+        elif re.match('P',char):
+            if
+        else:
+            pointer +=1
 
-if __name__ == '__main__':
-    main()
+    print(line_tokens)
+    tokens.append(line_tokens)
+    line_num+=1
